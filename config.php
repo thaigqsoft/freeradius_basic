5e22d5c6d210a90a516eb3711ee8b45f5f877cc0
Tanakorn Piamsin <yim@MacBook-Pro-khxng-Tanakorn.local>
2019-01-03 17:06:04 +0700

    start project

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+$DB_IP="127.0.0.1";
+$DB_USER="radius";
+$DB_PASS="1@12347!";
+$DB_NAME="radius";
+
+$conn = mysqli_connect($DB_IP, $DB_USER, $DB_PASS);
+   
+         if(! $conn ){
+            die('Could not connect: ' . mysqli_error());
+            exit();
+         }
+         mysqli_select_db( $DB_NAME );
+ 
+ 
---
+++ index.php
@@ -0,0 +1,11 @@
+<h1>Freeradius</h1> 
+   <h2>User</h2>
+   <a href="add_user.php">add_user</a><br>
+   <a href="list_user.php">list_user </a><br>
+   <a href="edit_user.php"> edit_user</a><br>
+   <a href="delete_user.php">delete_user</a><br>
+   <h2>Group</h2>
+   <a href="add_group.php">add_group</a><br>
+   <a href="list_group.php"> list_group</a><br>
+   <a href="edit_group.php"> edit_group</a><br>
+   <a href="delete_group.php">delete_group</a><br>
