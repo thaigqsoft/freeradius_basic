3731afc67b0a3164c012feabaeb2d79541f65a81
Tanakorn Piamsin <yim@MacBook-Pro-khxng-Tanakorn.local>
2019-01-03 17:45:35 +0700

    add group x

--- config.php
+++ config.php
@@ -4,12 +4,12 @@ $DB_USER="radius";
 $DB_PASS="1@12347!";
 $DB_NAME="radius";
 
-$conn = mysqli_connect($DB_IP, $DB_USER, $DB_PASS);
+$conn = mysqli_connect($DB_IP, $DB_USER, $DB_PASS,$DB_NAM);
    
          if(! $conn ){
             die('Could not connect: ' . mysqli_error());
             exit();
          }
-         mysqli_select_db( $DB_NAME );
+    //     mysqli_select_db( $DB_NAME );
  
  
