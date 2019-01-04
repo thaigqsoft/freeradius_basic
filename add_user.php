4a5969eef0b8a82befb51ffa0044707ad57ed704
Tanakorn Piamsin <yim@MacBook-Pro-khxng-Tanakorn.local>
2019-01-04 21:46:13 +0700

    add user

--- add_user.php
+++ add_user.php
@@ -1,18 +1,15 @@
 <?php
-include("config.php");
-if($_POST['Submit']=="Submit"){
+include "config.php";
+if ($_POST['Submit'] == "Submit") {
 
-   
+    $sql1 = "INSERT INTO radcheck(username,attribute,op,value)
+                       VALUES ( '" . $_POST['username'] . "','Cleartext-Password',':=','" . $_POST['pass'] . "' )";
+    mysqli_query($conn, $sql1) or die('Could not connect: ' . mysqli_error());
 
-    $sql1 = "INSERT INTO radcheck(username,attribute,op,value)  
-                       VALUES ( '".$_POST['username']."','Cleartext-Password',':=','".$_POST['pass']."' )";
-     mysqli_query($conn, $sql1) or   die('Could not connect: ' . mysqli_error());
+    $sql1 = "INSERT INTO radusergroup(groupname,username,priority)
+     VALUES ( '" . $_POST['group_name'] . "',':=','" . $_POST['username'] . "','1' )";
+    mysqli_query($conn, $sql1) or die('Could not connect: ' . mysqli_error());
 
-     $sql1 = "INSERT INTO radusergroup(groupname,username,priority)  
-     VALUES ( '".$_POST['group_name']."',':=','".$_POST['username']."','1' )";
-mysqli_query($conn, $sql1) or   die('Could not connect: ' . mysqli_error());   
-
-   
     $conn->close();
 
     echo "<br><br><a href='index.php'>INDEX</a><br>";
@@ -25,19 +22,16 @@ mysqli_query($conn, $sql1) or   die('Could not connect: ' . mysqli_error());
   <input type="text" name="username" value=""><br><br>
   Pass:
   <input type="text" name="pass" value=""><br><br>
+
   <select name="group_name" >
- 
   <?php
-  $sql = "SELECT * FROM radgroupreply group by groupname order by groupname";
-  $query = mysqli_query($conn,$sql);
- 
-while($result=mysqli_fetch_array($query,MYSQLI_ASSOC))
- { ?>
-  <option value="<?php echo $result["groupname"];?>"><?php echo $result["groupname"];?></option>
- 
-<?php }
- 
-?>
+$sql = "SELECT * FROM radgroupreply group by groupname order by groupname";
+$query = mysqli_query($conn, $sql);
+
+while ($result = mysqli_fetch_array($query, MYSQLI_ASSOC)) {?>
+  <option value="<?php echo $result["groupname"]; ?>"><?php echo $result["groupname"]; ?></option>
+
+<?php }?>
 </select><br><br>
   <input type="submit" name="Submit"  value="Submit">
 </form>
